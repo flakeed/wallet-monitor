a4b2fa2a2bdac34c527057b5c98571515df9155f
flakeed <flakeeed@gmail.com>
2025-08-01 15:58:09 +0300

    Update WebhookService path

--- server/src/server.js
+++ server/src/server.js
@@ -6,7 +6,7 @@ const { redis } = require('./services/tokenService');
 
 const WalletMonitoringService = require('./services/monitoringService');
 const Database = require('./database/connection');
-const WebhookService = require('./webhookService');
+const WebhookService = require('./services/WebhookService');
 const app = express();
 const port = process.env.PORT || 5001;
 
