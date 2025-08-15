2ee2a4c00af0e5040273bceae23866741c11437f
flakeed <flakeeed@gmail.com>
2025-08-15 17:27:51 +0300

    Add unrealized pnl

--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,7 @@ const { redis } = require('./services/tokenService');
 const WalletMonitoringService = require('./services/monitoringService');
 const Database = require('./database/connection');
 const SolanaWebSocketService = require('./services/solanaWebSocketService');
-
+const DexScreenerService = require('./services/dexScreenerService');
 const app = express();
 const port = process.env.PORT || 5001;
 
