145c74d004adbbd4375bae83673b470fd509bc15
flakeed <flakeeed@gmail.com>
2025-08-27 16:54:56 +0300

    Import useeffect

--- client/src/components/TokenTracker.js
+++ client/src/components/TokenTracker.js
@@ -1,5 +1,5 @@
 import TokenCard from './TokenCard';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function TokenTracker({ groupId, transactions, timeframe }) {
 const [items, setItems] = useState([]);
